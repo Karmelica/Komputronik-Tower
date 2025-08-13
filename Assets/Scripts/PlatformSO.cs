caa56f2d50521363c687a1cc215b31a378e52d39
SpeedAmigo <Kxipi7@gmail.com>
2025-08-13 14:50:16 +0200

    Platform rework wip

---
+++ Assets/Scripts/PlatformSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlatformSO", menuName = "Scriptable Objects/PlatformSO")]
+public class PlatformSO : ScriptableObject
+{
+    [Header("Platform Settings")]
+    public Sprite sprite;
+    public float durationToFall;
+    
+    [Header("Material settigns")]
+    [Range(0f,1f)] public float friction;
+    [Range(0f,1f)] public float bounciness;
+}
