619e46ec1952c30fed53a1629955320d462e99a3
SpeedAmigo <Kxipi7@gmail.com>
2025-08-19 12:03:20 +0200

    platform detector

--- Assets/PLatformDetector.cs
+++ Assets/PLatformDetector.cs
@@ -1,32 +1,60 @@
 using UnityEngine;
 
-public class PLatformDetector : MonoBehaviour
+public class PlatformDetector : MonoBehaviour
 {
-    [SerializeField] private float offset;
-    [SerializeField] private Transform player;
-    [SerializeField] private Transform gracePoint;
+    [Header("Dependencies")]
+    [SerializeField] private Character player;
+    [SerializeField] private Transform startPoint;
+    
+    [Header("Settings")]
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float graceTime;
+    public float speed;
 
-    //private Collider2D collider;
+    private bool _moveStarted;
+    private float _graceTimer;
+
+    private Collider2D _collider;
+    private Rigidbody2D _body;
     
     private void Awake()
     {
-        //collider = GetComponent<Collider2D>();
-        //collider.enabled = false;
-        transform.position = new Vector3(transform.position.x, player.position.y - offset, transform.position.z);
+        _body = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+        
+        _collider.enabled = false;
+        transform.position = startPoint.position;
+        _moveStarted = false;
     }
     
     private void Update()
     {
-        transform.position = new Vector3(transform.position.x, player.position.y - offset, transform.position.z);
-
-        /*if (transform.position.y >= gracePoint.position.y)
+        if (player.transform.position.y >= startPoint.position.y && !_moveStarted)
         {
-            collider.enabled = true;
-        }*/
-    }
-    
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        //Debug.Log(collision.gameObject.name);
+            _moveStarted = true;
+            _graceTimer = graceTime;
+        }
+        
+        if (_moveStarted)
+        {
+            if (_graceTimer > 0)
+            {
+                _graceTimer -= Time.deltaTime;
+                return;
+            }
+            
+            float targetY = player.transform.position.y - maxDistance;
+            
+            if (Mathf.Abs(transform.position.y - player.transform.position.y) < maxDistance)
+            {
+                _body.linearVelocity = new Vector2(0, speed);
+            }
+            else
+            {
+                _body.linearVelocity = Vector2.zero;
+                transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
+            }
+            _collider.enabled = true;
+        }
     }
 }
