ccc352d8d4b66d2ec088d75cf93e7d4ccf76ea90
SpeedAmigo <Kxipi7@gmail.com>
2025-09-02 10:54:17 +0200

    camera move change

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -2,11 +2,33 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public GameObject target; // The target object to follow
+    [SerializeField] private float offset;
+    [SerializeField] private float smoothTime = 0.2f;
     
-    // Update is called once per frame
-    void Update()
+    public GameObject target; // The target object to follow
+
+    private float _velocityY = 0f;
+    
+    private void LateUpdate()
     {
-        transform.position = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
+        if (target == null) return;
+
+        float targetY = transform.position.y;
+        float distanceY = target.transform.position.y - transform.position.y;
+
+        if (distanceY > offset)
+        {
+            targetY = target.transform.position.y - offset;
+        }
+        
+        else if (distanceY < -offset)
+        {
+            targetY = target.transform.position.y + offset;
+        }
+        
+        //transform.position = camPos;
+        float newY = Mathf.SmoothDamp(transform.position.y, targetY, ref _velocityY, smoothTime);
+        
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
     }
 }
--- Assets/Scripts/ColorBackgroundScript.cs
+++ Assets/Scripts/ColorBackgroundScript.cs
@@ -4,7 +4,7 @@ public class ColorBackgroundScript : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float offset;
-
+    
     private void Update()
     {
         transform.position = new Vector2(transform.position.x, player.position.y + offset);
